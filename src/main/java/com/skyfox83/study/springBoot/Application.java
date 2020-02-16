245bd3bc38763333d2829e5750965a212f1589c8
KBS <chp320@naver.com>
2020-02-16 09:24:09 +0900

    머스테치 설정

--- src/main/java/com/skyfox83/study/springBoot/Application.java
+++ src/main/java/com/skyfox83/study/springBoot/Application.java
@@ -1,7 +1,9 @@
 package com.skyfox83.study.springBoot;
 import org.springframework.boot.SpringApplication ;
 import org.springframework.boot.autoconfigure.SpringBootApplication ;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
