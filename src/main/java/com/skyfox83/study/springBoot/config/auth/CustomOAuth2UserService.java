1439e4ad487ea583d03f8e02df5950bd697c1981
KBS <chp320@naver.com>
2020-02-24 00:42:54 +0900

    5.3 구글 로그인 연동하기 < 수정/생성 소스 > - User.java - Role.java - UserRepository.java - build.gradle - SecurityConfig.java - CustomOAuth2UserService.java - OAuthAttributes.java - SessionUser.java ======================================================== (2/24) 1) CustomOAuth2UserService 내 loadUser() 메소드에서 user.getRoleKey() 인식 불가     ㄴ User클래스 보면 붉은 글씨로 표기 중

--- src/main/java/com/skyfox83/study/springBoot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/skyfox83/study/springBoot/config/auth/CustomOAuth2UserService.java
@@ -37,6 +37,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = saveOrUpdate(attributes) ;
         httpSession.setAttribute("user", new SessionUser(user)) ;
 
+
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRolekey())),
                                                                             attributes.getAttributes(),
                                                                             attributes.getNameAttributeKey()) ;
