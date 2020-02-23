2f9b35a50db64000942d50a2a5e1604532455ba5
KBS <chp320@naver.com>
2020-02-24 00:41:01 +0900

    5.3 구글 로그인 연동하기 < 수정/생성 소스 > - User.java - Role.java - UserRepository.java - build.gradle - SecurityConfig.java - CustomOAuth2UserService.java - OAuthAttributes.java - SessionUser.java ======================================================== (2/24) 1) CustomOAuth2UserService 내 loadUser() 메소드에서 user.getRoleKey() 인식 불가     ㄴ User클래스 보면 붉은 글씨로 표기 중

---
+++ src/main/java/com/skyfox83/study/springBoot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.skyfox83.study.springBoot.config.auth;
+
+import com.skyfox83.study.springBoot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserTypesOAuth2UserService customUserTypesOAuth2UserService ;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customUserTypesOAuth2UserService) ;
+
+    }
+}
