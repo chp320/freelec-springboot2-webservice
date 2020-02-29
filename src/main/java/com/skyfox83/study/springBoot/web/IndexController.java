2149b06bf9db5e2217a4927cafa0e85bdbe31f38
KBS <chp320@naver.com>
2020-03-01 03:02:25 +0900

    네이버 로그인 연동 - 네이버 로그인 실패

--- src/main/java/com/skyfox83/study/springBoot/web/IndexController.java
+++ src/main/java/com/skyfox83/study/springBoot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.skyfox83.study.springBoot.web;
 
+import com.skyfox83.study.springBoot.config.auth.LoginUser;
 import com.skyfox83.study.springBoot.config.auth.dto.SessionUser;
 import com.skyfox83.study.springBoot.service.posts.PostsService;
 import com.skyfox83.study.springBoot.web.dto.PostsResponseDto;
@@ -9,19 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService ;
-    private final HttpSession httpSession ;
+//    private final HttpSession httpSession ;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc()) ;
-        SessionUser user = (SessionUser) httpSession.getAttribute("user") ;
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user") ;
 
         if(user!=null) {
             model.addAttribute("userName", user.getName()) ;
