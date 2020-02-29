9b64fbc5cde8977f90742247907b214b4a461f7f
KBS <chp320@naver.com>
2020-03-01 03:23:53 +0900

    네이버 로그인 연동 - 네이버 로그인 실패

--- src/main/java/com/skyfox83/study/springBoot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/skyfox83/study/springBoot/config/auth/dto/OAuthAttributes.java
@@ -47,13 +47,19 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofNaver(String userNameAttributeName,
                                            Map<String, Object> attributes) {
-        Map<String, Object> response = (Map<String, Object>) attributes.get("name") ;
+//        Map<String, Object> response = (Map<String, Object>) attributes.get("response") ;
 
         return OAuthAttributes.builder()
-                .name((String) response.get("name"))
-                .email((String) response.get("email"))
-                .picture((String) response.get("profile_image"))
-                .attributes(response)
+//                .name((String) response.get("name"))
+//                .email((String) response.get("email"))
+//                .picture((String) response.get("profile_image"))
+//                .attributes(response)
+//                .nameAttributeKey(userNameAttributeName)
+//                .build() ;
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("profile_image"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build() ;
     }
